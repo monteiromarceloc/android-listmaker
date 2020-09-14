7774b8812aa04d6d5846673446f387a1cb8405b1
Marcelo Monteiro <monteiromarceloc@gmail.com>
2020-09-14 15:53:30 -0300

    create fragment

--- app/src/main/java/com/example/listmaker/MainActivity.kt
+++ app/src/main/java/com/example/listmaker/MainActivity.kt
@@ -12,10 +12,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() , TodoListAdapter.TodoListClickListener {
+class MainActivity : AppCompatActivity() , TodoListFragment.OnFragmentInteractionListener {
 
-    private lateinit var todoListRecyclerView: RecyclerView
-    val listDataManager: ListDataManager = ListDataManager(this)
+    private var todoListFragment = TodoListFragment.newInstance()
 
     companion object {
         const val INTENT_LIST_KEY = "list"
@@ -27,14 +26,13 @@ class MainActivity : AppCompatActivity() , TodoListAdapter.TodoListClickListener
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        var lists = listDataManager.readLists()
-        todoListRecyclerView = findViewById(R.id.lists_recyclerview)
-        todoListRecyclerView.layoutManager = LinearLayoutManager(this)
-        todoListRecyclerView.adapter = TodoListAdapter(lists, this)
-
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { _ ->
             showCreateTodoListDialog()
         }
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, todoListFragment)
+            .commit()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -48,16 +46,10 @@ class MainActivity : AppCompatActivity() , TodoListAdapter.TodoListClickListener
         if (requestCode == LIST_DETAIL_REQUEST_CODE) {
             data?.let {
                 val list = data.getParcelableExtra<TaskList>(INTENT_LIST_KEY)!!
-                listDataManager.saveList(list)
-                updateLists()
+                todoListFragment.saveList(list)
             }
         }
 
-    }
-
-    private fun updateLists() {
-        val lists = listDataManager.readLists()
-        todoListRecyclerView.adapter = TodoListAdapter(lists, this)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -81,10 +73,8 @@ class MainActivity : AppCompatActivity() , TodoListAdapter.TodoListClickListener
         myDialog.setView(todoTitleEditText)
         myDialog.setPositiveButton(positiveButtonTitle) {
             dialog, _ ->
-                val adapter = todoListRecyclerView.adapter as TodoListAdapter
                 val list = TaskList(todoTitleEditText.text.toString())
-                listDataManager.saveList(list)
-                adapter.addList(list)
+                todoListFragment.addList(list)
                 dialog.dismiss()
                 showTaskListItems(list)
         }
@@ -97,7 +87,7 @@ class MainActivity : AppCompatActivity() , TodoListAdapter.TodoListClickListener
         startActivityForResult(taskListItem, LIST_DETAIL_REQUEST_CODE)
     }
 
-    override fun listItemClicked(list: TaskList) {
+    override fun onTodoListClicked(list: TaskList) {
         showTaskListItems(list)
     }
 
